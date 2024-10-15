54d063f72b636d8aedd7e3cba2e7c91d144942c8
Quintanilla1299 <suke12k17@gmail.com>
2024-10-14 21:45:26 -0600

    caso 21

--- ApiSIT/controllers/schema/person.schema.js
+++ ApiSIT/controllers/schema/person.schema.js
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const personSchema = z.object({
-  first_name: z.string().min(1, 'First name is required').max(50, 'First name must be less than 50 characters'),
-  last_name: z.string().min(1, 'Last name is required').max(50, 'Last name must be less than 50 characters'),
-  cedula: z.string().min(9, 'Cedula must be at least 9 characters long').max(20, 'Cedula must be less than 20 characters')
+  first_name: z.string().trim().min(1, 'First name is required').max(50, 'First name must be less than 50 characters'),
+  last_name: z.string().trim().min(1, 'Last name is required').max(50, 'Last name must be less than 50 characters'),
+  cedula: z.string().trim().min(9, 'Cedula must be at least 9 characters long').max(20, 'Cedula must be less than 20 characters')
 })
